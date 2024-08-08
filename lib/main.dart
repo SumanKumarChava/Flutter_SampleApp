6897135e28c03eb1a4476d6ae0de2217dffdf6bb
Suman Kumar <Suman.Chava@blueyonder.com>
2024-08-08 09:08:53 +0530

    Moving the code outside of Main.dart to make seperate widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import './pages/currency_converter_materialpage.dart';
 
 void main() {
   runApp(const MyApp());
@@ -9,12 +10,6 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-        home: Scaffold(
-            body: Center(
-                child: Text(
-      "Hello, world",
-      textDirection: TextDirection.ltr,
-    ))));
+    return const MaterialApp(home: CurrencyConverterMaterialpage());
   }
 }
---
+++ lib/pages/currency_converter_materialpage.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class CurrencyConverterMaterialpage extends StatelessWidget {
+  const CurrencyConverterMaterialpage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+        body: Center(
+            child: Text(
+      "Hello, world",
+      textDirection: TextDirection.ltr,
+    )));
+  }
+}
