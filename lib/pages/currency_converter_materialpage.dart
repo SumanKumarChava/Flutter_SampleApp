28fb2ecb0eee74bcb90db09507727e1fd842870f
Suman Kumar <Suman.Chava@blueyonder.com>
2024-08-11 17:14:23 +0530

    Coloredbox, Text, Textfield widgets added

--- lib/pages/currency_converter_materialpage.dart
+++ lib/pages/currency_converter_materialpage.dart
@@ -6,10 +6,28 @@ class CurrencyConverterMaterialpage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const Scaffold(
-        body: Center(
-            child: Text(
-      "Hello, world",
-      textDirection: TextDirection.ltr,
-    )));
+        body: ColoredBox(
+            color: Color.fromRGBO(255, 0, 0, 0.2),
+            child: Center(
+                child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Text(
+                  "Hello, world",
+                  textDirection: TextDirection.ltr,
+                  style: TextStyle(
+                      color: Colors.green,
+                      fontSize: 36,
+                      fontWeight: FontWeight.bold),
+                ),
+                TextField(
+                  decoration: InputDecoration(hintText: "Hello suman"),
+                  style: TextStyle(
+                    color: Colors.white,
+                  ),
+                )
+              ],
+            ))));
   }
 }
